eb90dfae1e572b8568efa60f7bb51c6126fb9670
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-05 19:25:49 +0330

    kj

--- script.js
+++ script.js
@@ -76,6 +76,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll("#movie-list .card");
+    const currentPath = window.location.pathname;
+
+    cards.forEach(card => {
+        if (currentPath.includes("/movies") && !card.getAttribute("href").includes("movies")) {
+            card.style.display = "none";
+        } else if (currentPath.includes("/series") && !card.getAttribute("href").includes("series")) {
+            card.style.display = "none";
+        }
+    });
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.querySelector('#search-input');
     const searchButton = document.querySelector('#search-button');
