450e102260184e3b5228904bf559e772731d254e
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-07 15:45:10 +0330

    jh

--- script.js
+++ script.js
@@ -1,159 +1,282 @@
-let currentPage = 1;  // صفحه جاری
-const cardsPerPage = 1;  // تعداد کارت‌ها در هر صفحه
+let currentPage = 1;
+const cardsPerPage = 5;
 
-window.onload = function () {
-    // نمایش محتوای صفحه
+let sortOrder = {
+    latest: true,
+    'top-rated': true,
+    'site-rated': true
+};
+
+
+window.onload = function() {
     document.body.style.display = 'block';
+
     const skeletons = document.getElementById('skeletons');
     skeletons.style.display = 'none';
 
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.style.display = 'block';
+    });
+
     const params = new URLSearchParams(window.location.search);
-    currentPage = params.get('page') ? parseInt(params.get('page')) : 1; // صفحه از URL گرفته می‌شود
-    applyFilters();
-    showPage(currentPage);  // صفحه مورد نظر را نمایش می‌دهیم
+    const pageParam = params.get('page');
+    currentPage = pageParam ? parseInt(pageParam) : 1;
+
+    showPage(currentPage);
+
+    // کد فیلتر کردن بر اساس URL
+    const currentPath = window.location.pathname;
+    cards.forEach(card => {
+        const href = card.getAttribute("href");
+        if (currentPath.includes("/movies") && !href.includes("movies")) {
+            card.style.display = "none";
+        } else if (currentPath.includes("/series") && !href.includes("series")) {
+            card.style.display = "none";
+        } else if (currentPath.includes("/anime") && !href.includes("anime")) {
+            card.style.display = "none";
+        } else if (currentPath.includes("/irani") && !href.includes("irani")) {
+            card.style.display = "none";
+        }
+    });
 };
 
-function applyFilters() {
-    const searchTerm = document.querySelector('#search-input').value.toLowerCase();
-    const selectedFilter = document.querySelector('#filter-select').value.toLowerCase();
-    const cards = document.querySelectorAll('.card');
 
-    cards.forEach(card => {
-        const title = card.querySelector('h4').textContent.toLowerCase();
-        const href = card.getAttribute('href');
-        
-        // دسته‌بندی کارت‌ها
-        const isMovie = href.includes('movies');
-        const isSeries = href.includes('series');
-        const isAnime = href.includes('anime');
-        const isIrani = href.includes('irani');
+document.addEventListener('DOMContentLoaded', function () {
+    // چک می‌کند که آیا تم قبلاً ذخیره شده است یا خیر
+    const savedTheme = localStorage.getItem('theme') || 'retro';
+    document.documentElement.setAttribute('data-theme', savedTheme);
 
-        // بررسی مطابقت با فیلتر
-        let matchesCategory = false;
-        if (selectedFilter === 'all') {
-            matchesCategory = true;  // همه‌ی دسته‌ها را نشان می‌دهد
-        } else if (selectedFilter === 'movies' && isMovie) {
-            matchesCategory = true;
-        } else if (selectedFilter === 'series' && isSeries) {
-            matchesCategory = true;
-        } else if (selectedFilter === 'anime' && isAnime) {
-            matchesCategory = true;
-        } else if (selectedFilter === 'irani' && isIrani) {
-            matchesCategory = true;
-        }
+    // تغییر وضعیت چک‌باکس بر اساس تم ذخیره شده
+    const themeController = document.getElementById('theme-controller');
+    themeController.checked = savedTheme === 'forest';
 
-        const matchesSearch = title.includes(searchTerm);
-        
-        // اگر دسته‌بندی و جستجو مطابقت داشتند، نمایش داده می‌شود
-        if (matchesCategory && matchesSearch) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
+    // اضافه کردن رویداد به چک‌باکس
+    themeController.addEventListener('change', function () {
+        const newTheme = themeController.checked ? 'forest' : 'retro';
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector('#search-input');
+    const searchButton = document.querySelector('#search-button');
+
+    // تابعی برای فعال یا غیرفعال کردن دکمه جستجو
+    function toggleSearchButton() {
+        searchButton.disabled = !searchInput.value.trim();
+    }
+
+    // رویدادها
+    searchInput.addEventListener('input', function() {
+        toggleSearchButton(); // وضعیت دکمه را با توجه به ورودی تغییر دهید
+    });
+    
+    searchInput.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            filterMovies();
         }
     });
 
-    // بعد از اعمال فیلترها، صفحه اول را نشان می‌دهیم
-    showPage(1);
-}
+    searchButton.addEventListener('click', function() {
+        filterMovies();
+    });
 
-function showPage(page) {
-    const cards = Array.from(document.querySelectorAll('.card'));
-    const totalPages = Math.ceil(cards.length / cardsPerPage);  // تعداد کل صفحات
+    // غیرفعال کردن دکمه جستجو در ابتدا
+    toggleSearchButton();
+});
 
-    // جلوگیری از رفتن به صفحه‌های نامعتبر
-    if (page < 1 || page > totalPages) return;
 
-    // مخفی کردن همه کارت‌ها
-    cards.forEach(card => card.style.display = 'none');
 
-    // محاسبه و نمایش کارت‌ها برای صفحه انتخابی
+function showPage(page, filteredMovies = null) {
+    const movieList = filteredMovies || Array.from(document.querySelectorAll('#movie-list .card'));
+    const displayedCards = Math.min(movieList.length, cardsPerPage); // تعداد کارت‌های نمایش داده‌شده
+    const totalPages = Math.ceil(movieList.length / cardsPerPage);
+
+    document.getElementById('movie-list').scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+    movieList.forEach(card => card.style.display = 'none');
+
     const start = (page - 1) * cardsPerPage;
-    const end = start + cardsPerPage;
-
-    for (let i = start; i < end && i < cards.length; i++) {
-        cards[i].style.display = 'block';
+    const end = start + displayedCards; // استفاده از تعداد کارت‌های فیلتر شده
+    for (let i = start; i < end && i < movieList.length; i++) {
+        movieList[i].style.display = 'block';
     }
 
-    currentPage = page;  // به‌روزرسانی صفحه جاری
-    renderPagination(totalPages);  // به‌روزرسانی دکمه‌های صفحه‌بندی
+    currentPage = page; 
+    renderPagination(totalPages);
 
-    // به‌روزرسانی URL برای ذخیره صفحه
     window.history.pushState({ page: page }, '', `?page=${page}`);
 }
 
+
 function renderPagination(totalPages) {
     const paginationContainer = document.getElementById('paginationNumbers');
-    paginationContainer.innerHTML = '';  // پاک کردن شماره‌های صفحه قبلی
+    paginationContainer.innerHTML = '';
 
-    // ایجاد شماره‌های صفحه
-    for (let i = 1; i <= totalPages; i++) {
-        const pageLink = document.createElement('span');
-        pageLink.innerText = i;
-        pageLink.classList.add('page-number');
-        if (i === currentPage) pageLink.classList.add('active-page');
-        pageLink.onclick = () => showPage(i);
-        paginationContainer.appendChild(pageLink);
+    const pageNumbers = [];
+    if (currentPage > 1) pageNumbers.push(currentPage - 1);
+    pageNumbers.push(currentPage);
+    if (currentPage < totalPages) pageNumbers.push(currentPage + 1);
+    
+    if (currentPage > 2) {
+        pageNumbers.unshift(1);
     }
 
-    // مدیریت دکمه‌های قبلی و بعدی
+    if (currentPage < totalPages - 1) {
+        pageNumbers.push(totalPages);
+    }
+
+    const uniquePageNumbers = [...new Set(pageNumbers)];
+    const finalPages = [];
+
+    for (let i = 0; i < uniquePageNumbers.length; i++) {
+        finalPages.push(uniquePageNumbers[i]);
+
+        if (i < uniquePageNumbers.length - 1 && uniquePageNumbers[i + 1] - uniquePageNumbers[i] > 1) {
+            finalPages.push('...');
+        }
+    }
+
+    finalPages.forEach(page => {
+        if (page === '...') {
+            const dots = document.createElement('span');
+            dots.innerText = '...';
+            paginationContainer.appendChild(dots);
+        } else {
+            const pageLink = document.createElement('span');
+            pageLink.innerText = page;
+            pageLink.classList.add('page-number');
+            if (page === currentPage) pageLink.classList.add('active-page');
+
+            pageLink.onclick = () => changePage(page);
+            paginationContainer.appendChild(pageLink);
+        }
+    });
+
+    // اضافه کردن رویداد برای دکمه‌های قبلی و بعدی
     const prevPageButton = document.getElementById('prevPage');
     const nextPageButton = document.getElementById('nextPage');
 
     prevPageButton.disabled = currentPage === 1;
     nextPageButton.disabled = currentPage === totalPages;
 
-    prevPageButton.onclick = () => showPage(currentPage - 1);
-    nextPageButton.onclick = () => showPage(currentPage + 1);
+    prevPageButton.onclick = () => {
+        if (currentPage > 1) {
+            changePage(currentPage - 1);
+        }
+    };
+
+    nextPageButton.onclick = () => {
+        if (currentPage < totalPages) {
+            changePage(currentPage + 1);
+        }
+    };
 }
+
+function changePage(page) {
+    const skeletons = document.getElementById('skeletons');
+    const movieList = document.getElementById('movie-list');
+
+    // کارت‌ها را مخفی می‌کند
+    movieList.style.display = 'none';
+    skeletons.style.display = 'flex';
+
+    setTimeout(() => {
+        skeletons.style.display = 'none'; // اسکلت‌ها را مخفی می‌کند
+        movieList.style.display = 'grid'; // کارت‌ها را نمایش می‌دهد
+        showPage(page);
+    }, 1000); // یک ثانیه تاخیر قبل از نمایش کارت‌ها
+}
+
 
 function filterMovies() {
     const searchTerm = document.querySelector('#search-input').value.toLowerCase();
     const selectedFilter = document.querySelector('#filter-select').value.toLowerCase();
-    const movies = Array.from(document.querySelectorAll('.card'));
+    const movies = document.querySelectorAll('#movie-list .card');
 
-    const filteredMovies = movies.filter(movie => {
-        const title = movie.querySelector('h4').textContent.toLowerCase();
-        const href = movie.getAttribute('href');
-        
-        // دسته‌بندی‌ها
-        const isMovie = href.includes('movies');
-        const isSeries = href.includes('series');
-        const isAnime = href.includes('anime');
-        const isIrani = href.includes('irani');
+    let matchedMovies = [];
 
-        let matchesCategory = false;
-        if (selectedFilter === 'all') {
-            matchesCategory = true;  // همه‌ی دسته‌ها را نشان می‌دهد
-        } else if (selectedFilter === 'movies' && isMovie) {
-            matchesCategory = true;
-        } else if (selectedFilter === 'series' && isSeries) {
-            matchesCategory = true;
-        } else if (selectedFilter === 'anime' && isAnime) {
-            matchesCategory = true;
-        } else if (selectedFilter === 'irani' && isIrani) {
-            matchesCategory = true;
-        }
-
-        const matchesSearch = title.includes(searchTerm);
-        
-        return matchesCategory && matchesSearch;
-    });
-
-    // مخفی کردن کارت‌ها و نمایش اسکلت‌ها
+    // پنهان کردن کارت‌ها و نشان دادن اسکلت‌ها
     document.getElementById('movie-list').classList.add('hidden');
     document.getElementById('skeletonsx').classList.remove('hidden');
 
-    setTimeout(function () {
-        filteredMovies.forEach(movie => movie.style.display = 'block');
+    // تأخیر یک ثانیه‌ای برای نشان دادن اسکلت‌ها
+    setTimeout(function() {
+        movies.forEach(movie => {
+            const title = movie.querySelector('h4').textContent.toLowerCase();
+            
+            let category = '';
+            if (movie.href.includes('movies')) {
+                category = 'movies';
+            } else if (movie.href.includes('series')) {
+                category = 'series';
+            } else if (movie.href.includes('anime')) {
+                category = 'anime';
+            } else if (movie.href.includes('irani')) {
+                category = 'irani';
+            }
+
+            const matchesFilter = (selectedFilter === 'all') || (selectedFilter === category);
+            const matchesSearch = title.includes(searchTerm);
+
+            if (matchesFilter && matchesSearch) {
+                matchedMovies.push(movie);
+                movie.style.display = 'block';
+            } else {
+                movie.style.display = 'none';
+            }
+        });
+
         // پنهان کردن اسکلت‌ها و نشان دادن کارت‌ها
         document.getElementById('skeletonsx').classList.add('hidden');
         document.getElementById('movie-list').classList.remove('hidden');
 
         // به صفحه اول برگردید و نتایج فیلتر شده را نمایش دهید
-        showPage(1);
-    }, 1000); // یک ثانیه تاخیر
+        showPage(1, matchedMovies);
+    }, 1000); // یک ثانیه
+}
+
+
+
+function sortMovies(criteria) {
+    const movieList = document.getElementById('movie-list');
+    const movies = Array.from(movieList.children);
+
+    movies.forEach(movie => movie.classList.add('fade-out'));
+
+    setTimeout(() => {
+        movies.sort((a, b) => {
+            const aValue = criteria === 'latest' ? a.getAttribute('data-update') :
+                            criteria === 'top-rated' ? a.getAttribute('data-rating') :
+                            a.getAttribute('data-site-rating');
+
+            const bValue = criteria === 'latest' ? b.getAttribute('data-update') :
+                            criteria === 'top-rated' ? b.getAttribute('data-rating') :
+                            b.getAttribute('data-site-rating');
+        
+        return sortOrder[criteria] ? bValue - aValue : aValue - bValue;
+    });
+
+    sortOrder[criteria] = !sortOrder[criteria];
+
+    movieList.innerHTML = '';
+        movies.forEach(movie => {
+            movie.classList.remove('fade-out');
+            movie.classList.add('fade-in');
+            movieList.appendChild(movie);
+        });
+
+    showPage(currentPage);
+    }, 500);
 }
 
 // رویدادها
 document.querySelector('#search-input').addEventListener('input', filterMovies);
-document.querySelector('#search-button').addEventListener('click', filterMovies);
+document.querySelector('#search-input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        filterMovies();
+    }
+});
