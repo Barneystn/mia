4ef4288de0b6bf8c70058540d4cf08bc35260d11
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-07 10:20:14 +0330

    uy

--- script.js
+++ script.js
@@ -88,6 +88,28 @@ document.addEventListener('DOMContentLoaded', () => {
     toggleSearchButton();
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return null;
+    }
+
+    const authToken = getCookie('authToken');
+    const decryptedToken = authToken ? CryptoJS.AES.decrypt(authToken, 'secret-key').toString(CryptoJS.enc.Utf8) : null;
+    const myFilesSection = document.getElementById('my-files');
+    const loginButton = document.getElementById('login-button');
+
+    if (decryptedToken === 'nimleecher7') {
+        myFilesSection.style.display = 'block';
+        loginButton.style.display = 'none';
+    } else {
+        myFilesSection.style.display = 'none';
+        loginButton.style.display = 'inline-block';
+    }
+});
+
 function showPage(page, filteredMovies = null) {
     const movieList = filteredMovies || Array.from(document.querySelectorAll('#movie-list .card'));
     const displayedCards = Math.min(movieList.length, cardsPerPage); // تعداد کارت‌های نمایش داده‌شده
