450b671a95b388373490fcb565192fb26403f1ba
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-07 14:18:10 +0330

    تع

--- script.js
+++ script.js
@@ -9,16 +9,6 @@ let sortOrder = {
 
 
 window.onload = function() {
-    document.body.style.display = 'block';
-
-    const skeletons = document.getElementById('skeletons');
-    skeletons.style.display = 'none';
-
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(card => {
-        card.style.display = 'block';
-    });
-
     const params = new URLSearchParams(window.location.search);
     const pageParam = params.get('page');
     currentPage = pageParam ? parseInt(pageParam) : 1;
