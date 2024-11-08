6b09855793a28b2cb0d54d7c79f83f0b6292d5f4
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-08 17:37:45 +0330

    o

--- new.js
+++ new.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // چک می‌کند که آیا تم قبلاً ذخیره شده است یا خیر
-    const savedTheme = localStorage.getItem('theme') || 'retro';
-    document.documentElement.setAttribute('data-theme', savedTheme);
-
-    // تغییر وضعیت چک‌باکس بر اساس تم ذخیره شده
-    const themeController = document.getElementById('theme-controller');
-    themeController.checked = savedTheme === 'forest';
-
-    // اضافه کردن رویداد به چک‌باکس
-    themeController.addEventListener('change', function () {
-        const newTheme = themeController.checked ? 'forest' : 'retro';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-    });
-
-});
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const searchInput = document.getElementById("search-input");
     const searchButton = document.getElementById("search-button");
